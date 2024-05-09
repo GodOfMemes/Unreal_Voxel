7c6444014691508e43952c3892962527dc339a3d
GodOfMemes <>
2024-05-08 21:51:59 -0400

    Updated to 5.4

--- Source/UE5VoxelTutorial.Target.cs
+++ Source/UE5VoxelTutorial.Target.cs
@@ -8,7 +8,8 @@ public class UE5VoxelTutorialTarget : TargetRules
 	public UE5VoxelTutorialTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.AddRange( new string[] { "UE5VoxelTutorial" } );
 	}
 }
--- Source/UE5VoxelTutorialEditor.Target.cs
+++ Source/UE5VoxelTutorialEditor.Target.cs
@@ -8,7 +8,8 @@ public class UE5VoxelTutorialEditorTarget : TargetRules
 	public UE5VoxelTutorialEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.AddRange( new string[] { "UE5VoxelTutorial" } );
 	}
 }
